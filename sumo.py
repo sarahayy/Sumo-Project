e4ae571657239dc572c0589ea2f3050017b286b7
NathanX <XDN2369@hotmail.com>
2018-03-22 23:01:17 +1100

    sumo.py

---
+++ sumo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from time import sleep
+
+import ev3dev.ev3
+import sys, os
+
+#Connect motors
+rightMotor = LargeMotor(OUTPUT_B)
+leftMotor  = LargeMotor(OUTPUT_C)
+
+# Connect touch sensors.
+"""
+ts1 = TouchSensor(INPUT_1);	assert ts1.connected
+ts4 = TouchSensor(INPUT_4);	assert ts4.connected
+us  = UltrasonicSensor();	assert us.connected
+gs  = GyroSensor();		assert gs.connected
+
+gs.mode = 'GYRO-RATE'	# Changing the mode resets the gyro
+gs.mode = 'GYRO-ANG'	# Set gyro mode to return compass angle
+"""
