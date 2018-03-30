7c8c1ab51b34a043f971f6a108609348896d69ae
NathanX <XDN2369@hotmail.com>
2018-03-30 15:29:37 +1100

    update

--- sumo.py
+++ sumo.py
@@ -5,12 +5,12 @@ import sys, os
 
 from ev3dev.ev3 import *
 
-#====================== MOTOR ======================#
+#========================= MOTOR =========================#
 
 rightMotor = LargeMotor(OUTPUT_B)
 leftMotor  = LargeMotor(OUTPUT_C)
 
-#===================== SENSORS =====================#
+#======================== SENSORS ========================#
 
 ts1 = TouchSensor(INPUT_1);	    assert ts1.connected
 ts4 = TouchSensor(INPUT_4);	    assert ts4.connected
@@ -19,29 +19,31 @@ us  = UltrasonicSensor();       assert us.connected
 gs  = GyroSensor();		        assert gs.connected
 
 gs.mode = 'GYRO-RATE'	# Changing the mode resets the gyro
-gs.mode = 'GYRO-ANG'	# Set gyro mode to return compass angle
+gs.mode = 'GYRO-ANG'	# Set mode to return compass angle
 
 cs.mode = "COL-REFLECT" # Set to sense reflected light
 
-#====================== BUTTON =====================#
-
+#========================= BUTTON ========================#
 # EV3 Button State
 
 btn = Button()
 
-#================== Move Straight ==================#
+#===================== Move Straight =====================#
+# Move Betsie straight forward
 
 def moveStraight():
     leftMotor.duty_cycle_sp = 75
     rightMotor.duty_cycle_sp = 75
 
-#====================== Stop =======================#
+#========================= Stop ==========================#
+# Stop Betsie
 
 def stop():
     rightMotor.stop(stop_action='brake')
     leftMotor.stop(stop_action='brake')
 
-#===================== Backup ======================#
+#======================== Backup =========================#
+# Back up Betsie for 2 seconds
 
 def backup():
     rightMotor.run_timed(speed_sp=-500, time_sp=2000)
@@ -49,7 +51,8 @@ def backup():
     rightMotor.wait_while('running')
     leftMotor.wait_while('running')
 
-#===================== Detect ======================#
+#======================== Detect =========================#
+# Turn until Betsie detects something close in front of it
 
 def initDetect(LR):
     distance = us.value();
@@ -68,16 +71,20 @@ def initDetect(LR):
         distance = us.value();
         print("Distance: " + str(distance))
 
-#================== Detect Colour ==================#
+#===================== Detect Colour =====================#
+# Stop and backup Betsie if the black line is detected
 
 def detectColour():
     print("Color: " + str(cs.value()))
     if cs.value() < 30:
         stop()
         backup()
+        #initDetect()
 
-#================== Left or Right ==================#
-# Determine direction to turn based on the button pressed
+#===================== Left or Right =====================#
+# Determine which direction to turn based on which button
+# is pressed (left/right)
+
 def checkLR():
     print("Choose left or right")
     while not btn.any():
@@ -89,25 +96,23 @@ def checkLR():
             return "right"
 
 
-#================== Initial / Main ==================#
+#===================== Initial / Main =====================#
 
 print("Starting")
+Sound.speak("Starting").wait()
 
-
-while not btn.any():    # Waits for button press then sleep for 3 seconds
-    time.sleep(0.5)
+while not btn.any():    # Wait for a button press then sleep
+    time.sleep(0.5)     # for 3 seconds
 
 print("Sleeping")
 time.sleep(3)
 print("Finished sleeping")
 
-
-
 rightMotor.run_direct(duty_cycle_sp=0)
 leftMotor.run_direct(duty_cycle_sp=0)
 
 
-#LR = checkLR()          # Checks to turn left or right
+#LR = checkLR()     # Check to turn left or right
 LR = "right"
 
 initDetect(LR)
