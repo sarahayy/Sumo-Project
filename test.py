c5397512b8c18b107631d29fe263123d524582f7
NathanX <XDN2369@hotmail.com>
2018-03-30 15:35:21 +1100

    update

--- test.py
+++ test.py
@@ -16,9 +16,41 @@ def moveStraight():
     leftMotor.duty_cycle_sp = 75
     rightMotor.duty_cycle_sp = 75
 
+def moveStraightAlt():
+    leftMotor.run_forever(speed_sp=900)
+    rightMotor.run_forever(speed_sp=900)
+
 #====================== Stop =======================#
 
 def stop():
     rightMotor.stop(stop_action='brake')
     leftMotor.stop(stop_action='brake')
 
+#====================== Left =======================#
+
+def left():
+    leftMotor.duty_cycle_sp = -75
+    rightMotor.duty_cycle_sp = 75
+
+#===================== Right =======================#
+
+def right():
+    leftMotor.duty_cycle_sp = 75
+    rightMotor.duty_cycle_sp = -75
+
+#====================== Main =======================#
+
+btn = Button()
+
+while btn.any():
+    mode = input("Enter mode: ")
+    if mode == "straight":
+        moveStraight()
+    elif mode == "stop":
+        stop()
+    elif mode == "left":
+        left()
+    elif mode == "right":
+        right()
+    else:
+        print("Invalid input")
