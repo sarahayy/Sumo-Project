dc19d9d45ffaea9c540939a275235857b742f268
NathanX <XDN2369@hotmail.com>
2018-03-30 11:36:27 +1100

    update

--- sumo.py
+++ sumo.py
@@ -12,7 +12,9 @@ leftMotor  = LargeMotor(OUTPUT_C)
 # Connect touch sensors.
 #ts1 = TouchSensor(INPUT_1);	assert ts1.connected
 #ts4 = TouchSensor(INPUT_4);	assert ts4.connected
-us  = UltrasonicSensor();	assert us.connected
+cl = ColorSensor();            assert cl.connected
+us  = UltrasonicSensor();	    assert us.connected
+
 """
 gs  = GyroSensor();		assert gs.connected
 
@@ -34,11 +36,20 @@ while not btn.any():
 '''
 
 print("Sleeping")
-ev3.Sound.speak('ha ha').wait()
+
+#ev3.Sound.speak('ha ha').wait()
 
 time.sleep(3)
 
 print("Finished sleeping")
+
+
+def detectColor():
+    cl.mode = "COL-REFLECT"
+    print("Color: " + str(cl.value()))
+    if cl.value() < 30:
+        stop()
+        time.sleep(60)
 
 def check():
 
@@ -51,23 +62,46 @@ def check():
 
 def moveStraight():
 
-    rightMotor.run_direct(duty_cycle_sp=75)
-    leftMotor.run_direct(duty_cycle_sp=75)
+    rightMotor.duty_cycle_sp = 75
+    leftMotor.duty_cycle_sp = 75
 
 def detect():
-
     distance = us.value();
-    print(distance)
-    while distance > 10:
-        rightMotor.run_direct(duty_cycle_sp=10)
+
+    while distance > 370:
+
+        '''
+        if LoR == "right":
+            leftMotor.duty_cycle_sp = 10
+        else if LoR == "left":
+            rightMotor.duty_cycle_sp = 10
+        '''
+
+        distance = us.value();
+        print("Distance: " + str(distance))
+        leftMotor.duty_cycle_sp = 0
+        leftMotor.duty_cycle_sp = 75
 
 def stop():
     rightMotor.stop(stop_action='brake')
     leftMotor.stop(stop_action='brake')
 
+#======================================#
+'''
+if btn.left:
+    LoR = "left"
+elif btn.right:
+    LoR = "right"
+'''
+
+rightMotor.run_direct(duty_cycle_sp=0)
+leftMotor.run_direct(duty_cycle_sp=0)
+
 
 while not btn.any():
         detect()
         moveStraight()
+        #detectColor()
 
 
+#eeee
